eb818934ec6271921d8d155ad942dda0fb5f149b
phonghoangkhong <phongkhonghoang@gmail.com>
2020-04-25 08:29:54 +0700

    PPP

--- src/main/java/hbase/simple/ReadData.java
+++ src/main/java/hbase/simple/ReadData.java
@@ -7,6 +7,8 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 import java.io.IOException;
+import java.util.Random;
+
 /**
  * Author:PHONGKH
  * B1: connect hbase
@@ -16,27 +18,32 @@ import java.io.IOException;
 public class ReadData {
     public static void main(String args[]){
         try {
+             Random random=new Random();
+            while (true) {
+                ConnectionHbase connectionHbase = new ConnectionHbase();
+                Connection connection = connectionHbase.getConnection();
+                Table hTable = connection.getTable(TableName.valueOf("testhbase2"));
+                long d=System.currentTimeMillis();
+                for (int i = 0; i < 1000000; i++) {
+                    int rowkey=random.nextInt(100000000);
+                    Get g = new Get(Bytes.toBytes(rowkey));
+                    //read data
+                    Result result = hTable.get(g);
+                    // Reading values from Result class object
+                    byte[] value = result.getValue(Bytes.toBytes("cf1"), Bytes.toBytes("level"));
 
-            ConnectionHbase connectionHbase=new ConnectionHbase();
-            Connection connection=connectionHbase.getConnection();
-            System.out.println("OK");
-            Table hTable= connection.getTable(TableName.valueOf("emp"));
-            Get g=new Get(Bytes.toBytes("row1"));
-            //read data
-            Result result=hTable.get(g);
-            // Reading values from Result class object
-            byte [] value = result.getValue(Bytes.toBytes("personal data"),Bytes.toBytes("name"));
+                    byte[] value1 = result.getValue(Bytes.toBytes("cf2"), Bytes.toBytes("from_date"));
 
-            byte [] value1 = result.getValue(Bytes.toBytes("personal data"),Bytes.toBytes("city"));
+                    // Printing the values
+                    String level = Bytes.toString(value);
+                    String from_date= Bytes.toString(value1);
 
-            // Printing the values
-            String name = Bytes.toString(value);
-            String city = Bytes.toString(value1);
 
-            System.out.println("name: " + name + " city: " + city);
-             hTable.close();
-            connection.close();
-
+                }
+                hTable.close();
+                connection.close();
+                System.out.println(System.currentTimeMillis()-d);
+            }
         }catch(IOException e){
             e.printStackTrace();
         }
